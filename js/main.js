d6e131d15a00da68b5c4fe517d35819ac27647b1
h-takamori <hiromutakamori@gmail.com>
2018-11-14 12:01:56 +0900

    双方向データバインディングの練習

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 var app = new Vue({
     el: '#app',
+    data: {
+        newItem: '',
+        todos: []
+    },
     methods: {
         addItem: function(event) {
-            alert();
+            if(this.newItem == '') return;
+
+            var todo = {
+                item: this.newItem
+            };
+
+            this.todos.push(todo);
+            this.newItem = '';
         }
     }
 })
